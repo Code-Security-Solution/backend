d40dd8e99514060d8dec109122d4a8b68be06ba0
LKW00 <gjy035@naver.com>
2025-05-05 23:52:50 +0900

    상세리포트 api 추가

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import tempfile
 from handle_scan_request import scan_code
 from handle_get_result import get_scan_result
 from handle_get_summary import get_summary_report
+from handle_get_detail import get_detailed_report
 from handle_auth import token_required, register_user_handler, login_handler, get_user_info
 from handle_files import download_source_handler, download_result_handler, download_translated_result_handler, download_all_sources_handler, my_files_handler
 from database import register_user, get_user_by_email, get_user_files, get_file_by_id, json_serialize
@@ -34,6 +35,7 @@ app.json_encoder = MongoJSONEncoder  # 커스텀 JSONEncoder 설정
 app.add_url_rule('/scan', view_func=scan_code, methods=['POST'])
 app.add_url_rule('/scan-result/<file_id>', view_func=get_scan_result, methods=['GET'])
 app.add_url_rule('/summary-report/<file_id>', view_func=get_summary_report, methods=['GET'])
+app.add_url_rule('/detail-report/<file_id>', view_func=get_detailed_report, methods=['GET'])
 
 # 인증 관련 라우팅
 app.add_url_rule('/register', view_func=register_user_handler, methods=['POST'])
